fc7b7f4936ab478c1c07233eb55752d3e093d9d3
uluna99 <uluna099@gmail.com>
2024-12-12 13:16:39 +0100

    dia 12-12

--- Testeo2D 24-25/Assets/Testeo2D_Root/Scripts/PlayerController2D.cs
+++ Testeo2D 24-25/Assets/Testeo2D_Root/Scripts/PlayerController2D.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.XR;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController2D : MonoBehaviour
 {
@@ -9,9 +11,13 @@ public class PlayerController2D : MonoBehaviour
 
     //Referencias generales
     [SerializeField] Rigidbody2D playerRb; //Ref al rigidbody del player
+    [SerializeField] PlayerInput playerInput; //Ref al gestor del input del jugador
+    [SerializeField] Animator playerAnim; //ref al animator para gestionar las transiciones de animación
 
     [Header("Movement Parameters")]
+    private Vector2 moveInput; //Almacén del input del player
     public float speed;
+    [SerializeField] bool isFacingRight; 
 
     [Header("Jump Parameters")]
     public float jumpForce;
@@ -22,13 +28,31 @@ public class PlayerController2D : MonoBehaviour
     {
         //Autoreferenciar componentes: nombre de variable = GetComponent()
         playerRb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+        playerAnim = GetComponent<Animator>();
+        isFacingRight = true; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        horInput = Input.GetAxis("Horizontal");
-        Jump();
+        HandleAnimation();
+
+        //Flip
+        if (moveInput.x > 0)
+        {
+            if (!isFacingRight)
+            {
+                Flip();
+            }
+        }
+        if (moveInput.x < 0)
+        {
+            if (isFacingRight)
+            {
+                Flip();
+            }
+        }
     }
 
     private void FixedUpdate()
@@ -38,14 +62,50 @@ public class PlayerController2D : MonoBehaviour
 
     void Movement()
     {
-        playerRb.velocity = new Vector3(horInput * speed, playerRb.velocity.y, 0);
+        playerRb.velocity = new Vector3(moveInput.x * speed, playerRb.velocity.y, 0);
     }
 
-    void Jump()
+    void Flip()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        Vector3 CurrentScale = transform.localScale;
+        CurrentScale.x *= -1;
+        transform.localScale = CurrentScale;
+        isFacingRight = !isFacingRight; //nombre de bool = !nombre de bool (cambio al estado contrario)
+
+    }
+
+    void HandleAnimation()
+    {
+        //Conector de valores generales con parámetros de cambios de animación
+        playerAnim.SetBool("isJumping", !isGrounded);
+        if (moveInput.x > 0 || moveInput.x < 0) playerAnim.SetBool("isRunning", true);
+        else playerAnim.SetBool("isRunning", false);
+    }
+
+
+
+
+    #region Input Events 
+    //Para crear un evento:
+    //Se define PUBLIC sin tipo de dato (VOID) y con una referencia al input (Callback.Context)
+    public void HandleMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+
+    public void HandleJump(InputAction.CallbackContext context)
+    {
+        if (context.started)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
-        }
+        }   
+        
+       
     }
+
+
+
+
+    #endregion
 }
